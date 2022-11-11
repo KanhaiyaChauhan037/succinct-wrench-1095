006e836cc4bb976afe3522732e148f6a942eeff5
AnuPratap10 <anupratapsingh49@gmail.com>
2022-11-12 00:16:58 +0530

    day-4 mine

--- white-feather/src/Components/MyBag.jsx
+++ white-feather/src/Components/MyBag.jsx
@@ -14,9 +14,9 @@ const MyBag = () => {
     
 
           
-            <ChakraProvider>
+          
             <MyBagNavbar />
-            </ChakraProvider>
+          
         </div>
     )
 }
