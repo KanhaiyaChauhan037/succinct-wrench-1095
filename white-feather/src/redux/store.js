eec4e37cd88e97207f243991447112c60b565c7d
lalitUnstopable <lalitpawar229@gmail.com>
2022-11-12 00:57:40 +0530

    CartCreated

--- white-feather/src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore} from 'redux';
-import shopReducer from './shopReducer';
-import {composeWithDevTools} from 'redux-devtools-extension'
-
-const store = createStore(shopReducer,composeWithDevTools());
-
-export default store;
